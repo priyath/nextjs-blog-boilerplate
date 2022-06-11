c81a166d1de7632fcafd84245ee11a90a90ad2fa
priyath <priyath.gregory@gmail.com>
2022-06-11 20:49:50 +0800

    added dummy data

---
+++ data/dummyData.ts
@@ -0,0 +1,12 @@
+export const posts = [
+  {
+    title: 'My First Blog Post',
+    slug: 'first-post',
+    publishedDate: '02-06-2022',
+  },
+  {
+    title: 'My Second Blog Post',
+    slug: 'second-post',
+    publishedDate: '01-05-2022',
+  },
+];
