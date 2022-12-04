a8d45f907c3db7f5765f84eee99723d58c054b75
priyath <priyath.gregory@gmail.com>
2022-12-04 16:52:41 +0800

    Added custom components

---
+++ components/MDXComponents/CustomComponent/index.tsx
@@ -0,0 +1,9 @@
+import { Box } from '@mui/material';
+
+const CustomComponent = () => {
+  return <Box>
+    This is a custom component
+  </Box>
+}
+
+export default CustomComponent;
--- components/MDXComponents/MDXComponents.tsx
+++ components/MDXComponents/MDXComponents.tsx
@@ -1,7 +1,9 @@
 import BlogH2 from './H2';
+import CustomComponent from './CustomComponent';
 
 const MDXComponents = {
   h2: BlogH2,
+  CustomComponent,
 };
 
 export default MDXComponents;
